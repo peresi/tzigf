803354b64f6ded0a22d96790c7b47710da9af009
peresi <officialperec@outlook.com>
2026-04-07 13:22:31 +0300

    Add live Dar es Salaam clock in top bar

--- resources/views/partials/site-navigation.blade.php
+++ resources/views/partials/site-navigation.blade.php
@@ -1,5 +1,7 @@
 <div class="site-topbar">
-    <div class="topbar-left">Established 2020</div>
+    <div class="topbar-left">
+        <span id="tz-clock">Dar es Salaam Time: -- --- ----, --:--:-- EAT</span>
+    </div>
     <div class="topbar-center">
         <div class="topbar-title">Tanzania Internet Governance Forum</div>
         <div class="topbar-subtitle">Building inclusive, multistakeholder internet policy for Tanzania</div>
@@ -31,3 +33,33 @@
         @include('partials.site-navigation-links')
     </div>
 </nav>
+<script>
+    (function () {
+        const clockEl = document.getElementById('tz-clock');
+        if (!clockEl) return;
+
+        const formatter = new Intl.DateTimeFormat('en-GB', {
+            timeZone: 'Africa/Dar_es_Salaam',
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        });
+
+        function updateClock() {
+            const parts = formatter.formatToParts(new Date());
+            const map = {};
+            for (const part of parts) {
+                map[part.type] = part.value;
+            }
+
+            clockEl.textContent = `Dar es Salaam Time: ${map.day} ${map.month} ${map.year}, ${map.hour}:${map.minute}:${map.second} EAT`;
+        }
+
+        updateClock();
+        setInterval(updateClock, 1000);
+    })();
+</script>
